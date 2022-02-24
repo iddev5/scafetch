375c40d24f98dd7d6adeb89e38e38bf0ce804d54
iddev5 <ayushbardhan5@gmail.com>
2022-02-24 21:28:25 +0530

    Support evaluating projects to multiple git hosts

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,28 @@
 const std = @import("std");
 const json = std.json;
+const mem = std.mem;
+const allocPrint = std.fmt.allocPrint;
 const zfetch = @import("zfetch");
 const utils = @import("utils.zig");
 const TtyColor = utils.TtyColor;
+
+const Hosts = enum {
+    github,
+    gitlab,
+    codeberg,
+};
+
+const hosts_map = std.ComptimeStringMap(Hosts, .{
+    .{ "github.com", .github },
+    .{ "gitlab.com", .gitlab },
+    .{ "codeberg.com", .codeberg },
+    .{ "github", .github },
+    .{ "gitlab", .gitlab },
+    .{ "codeberg", .codeberg },
+    .{ "gh", .github },
+    .{ "gl", .gitlab },
+    .{ "cb", .codeberg },
+});
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -17,7 +37,30 @@ pub fn main() anyerror!void {
     try zfetch.init();
     defer zfetch.deinit();
 
-    const url = try std.fmt.allocPrint(allocator, "https://api.github.com/repos/{s}", .{args[1]});
+    const project = args[1];
+    const url = if (mem.startsWith(u8, project, "http:") or mem.startsWith(u8, project, "https:")) blk: {
+        break :blk try allocator.dupe(u8, project);
+    } else switch (std.mem.count(u8, project, "/")) {
+        1 => try allocPrint(allocator, "https://api.github.com/repos/{s}", .{project}),
+        2 => blk: {
+            var tokenizer = std.mem.tokenize(u8, project, "/");
+            const host_name = tokenizer.next().?;
+            const author_name = tokenizer.next().?;
+            const project_name = tokenizer.rest();
+
+            if (hosts_map.get(host_name)) |host| break :blk switch (host) {
+                .github => try allocPrint(allocator, "https://api.github.com/repos/{s}/{s}", .{ author_name, project_name }),
+                .gitlab => try allocPrint(allocator, "https://gitlab.com/api/v4/projects/{s}%2F{s}", .{ author_name, project_name }),
+                .codeberg => try allocPrint(allocator, "https://codeberg.org/api/v1/repos/{s}/{s}", .{ author_name, project_name }),
+            } else {
+                break :blk try allocPrint(allocator, "https://{s}", .{project});
+            }
+        },
+        else => {
+            std.log.err("malformed url/project name", .{});
+            std.process.exit(1);
+        },
+    };
     defer allocator.free(url);
 
     const source = try utils.requestGet(allocator, url);
