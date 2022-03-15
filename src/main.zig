0e7ba45478e236765cb699d4d0d0a5f7246d3217
iddev5 <ayushbardhan5@gmail.com>
2022-03-15 15:57:44 +0000

    Add --help command, changed host short command to -o

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,20 @@ const AyArgparse = @import("ay-arg");
 
 pub fn printVersion(writer: anytype) !void {
     try writer.writeAll("scafetch v0.1\n");
-    std.process.exit(0);
+}
+
+pub fn printHelp(writer: anytype) !void {
+    try printVersion(writer);
+    try writer.writeAll(
+        \\usage: scafetch <author>/<repository>
+        \\
+        \\  -o, --host <host>       Select the host server, accepted values are
+        \\                           github (default), gitlab and codeberg
+        \\  -h, --help              Show this help message
+        \\  -v, --version           Show the app version
+        \\
+        \\
+    );
 }
 
 pub fn main() anyerror!void {
@@ -22,21 +35,30 @@ pub fn main() anyerror!void {
     defer std.process.argsFree(allocator, args);
 
     const params = &[_]AyArgparse.ParamDesc{
-        .{ .long = "host", .short = "h", .need_value = true },
+        .{ .long = "host", .short = "o", .need_value = true },
         .{ .long = "version", .short = "v" },
+        .{ .long = "help", .short = "h" },
     };
     var ap = AyArgparse.init(allocator, params[0..]);
     defer ap.deinit();
     try ap.parse(args[1..]);
 
-    if (ap.arguments.get("version")) |_|
+    if (ap.arguments.get("version")) |_| {
         try printVersion(stdout);
+        std.process.exit(0);
+    }
+
+    if (ap.arguments.get("help")) |_| {
+        try printHelp(stdout);
+        std.process.exit(0);
+    }
 
     try zfetch.init();
     defer zfetch.deinit();
 
     const project = ap.positionals.items[0];
     var host_tag: Host = if (ap.arguments.get("host")) |h| std.meta.stringToEnum(Host, h) orelse {
+        try printHelp(stdout);
         std.log.err("unknown host name: {s}\n", .{h});
         std.process.exit(1);
     } else .default;
@@ -60,6 +82,7 @@ pub fn main() anyerror!void {
 
                 if (Host.match(host_name)) |host| {
                     if (host_tag != .default and host != host_tag) {
+                        try printHelp(stdout);
                         std.log.err("mismatched host name in argument and in url", .{});
                         std.process.exit(1);
                     }
@@ -68,6 +91,7 @@ pub fn main() anyerror!void {
                 }
             },
             else => {
+                try printHelp(stdout);
                 std.log.err("malformed url/project name", .{});
                 std.process.exit(1);
             },
